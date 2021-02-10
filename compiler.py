01bc33c16fd0f8890a122d35fd784ccef8838d54
luis <mentesniker@gmail.com>
2021-02-10 09:55:42 -0600

    added option to draw circuit

--- compiler.py
+++ compiler.py
@@ -6,10 +6,16 @@ try:
     else:
         raise FileNotFoundError("\n File name must be specified on argument 1 \n")
     newQc = QuantumCircuit.from_qasm_file(filename)
-    backend = BasicAer.get_backend('qasm_simulator')
     shots = 100
-    if(len(sys.argv) > 2):
+    if(len(sys.argv) > 2 and (sys.argv[2] == "d" or sys.argv[2] == "draw")):
+        print("\n")
+        print(newQc.draw())
+        print("\n")
+    elif(len(sys.argv) > 2):
         shots = int(sys.argv[2])
+    if(len(sys.argv) > 3):
+        shots = int(sys.argv[3])
+    backend = BasicAer.get_backend('qasm_simulator')
     job = execute(newQc, backend, shots=shots)
     result = job.result()
     print("\n The results are: " + str(result.get_counts(newQc)) + "\n")
