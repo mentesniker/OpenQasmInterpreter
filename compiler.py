dfef557361b5df6076584015ac9784ff10e7e37e
luis <mentesniker@gmail.com>
2021-02-10 09:44:56 -0600

    compiler completed

--- compiler.py
+++ compiler.py
@@ -1,12 +1,18 @@
 from qiskit import QuantumCircuit,execute,BasicAer
 import sys 
 try:
-    if(len(sys.argv) > 2 and sys.argv[2] == 'run'):
-        newQc = QuantumCircuit.from_qasm_file('circuit.qasm')
-        backend = BasicAer.get_backend('qasm_simulator')
-        job = execute(newQc, backend, shots=int(sys.argv[1]))
-        result = job.result()
-        print("\n The results are: " + str(result.get_counts(newQc)) + "\n")
+    if(len(sys.argv) > 1):
+        filename = str(sys.argv[1])
+    else:
+        raise FileNotFoundError("\n File name must be specified on argument 1 \n")
+    newQc = QuantumCircuit.from_qasm_file(filename)
+    backend = BasicAer.get_backend('qasm_simulator')
+    shots = 100
+    if(len(sys.argv) > 2):
+        shots = int(sys.argv[2])
+    job = execute(newQc, backend, shots=shots)
+    result = job.result()
+    print("\n The results are: " + str(result.get_counts(newQc)) + "\n")
 except Exception as inst:
     d = inst
     print(d)
